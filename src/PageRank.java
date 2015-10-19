51bae59cef47872e3c59740d5a6e9ba283ee2c0d
ZhouLin1992 <lzhou0922@ufl.edu>
2015-10-19 00:13:59 -0400

    Fixed bugs

--- src/PageRank.java
+++ src/PageRank.java
@@ -1,9 +1,7 @@
-//package org.apache.hadoop.examples;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
@@ -22,27 +20,30 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class PageRank {
-	// TODO nothing 
 	private static String N_sum = "";
-	private static int count = 0;
+	private static int iterCount = -1;
+	private static long oldConvergeCount = -1;
 
-	public static class CalNMapper 
+
+	public static class CalNMapper
 	extends Mapper<Object,// input key
 	Text,  // input value
 	Text,  // output key
 	IntWritable> //output value
-	{ 
+	{
 		private final static IntWritable one = new IntWritable(1);
-		public void map(Object key, Text value, Context context) 
+		@Override
+		public void map(Object key, Text value, Context context)
 				throws IOException, InterruptedException {
 			context.write (new Text("a"),one);
 		}
 	}
 
-	public static class CalNReducer 
+	public static class CalNReducer
 	extends Reducer<Text,IntWritable,IntWritable, NullWritable> {
 		private IntWritable result = new IntWritable();
-		public void reduce(Text key, Iterable<IntWritable> values, 
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values,
 				Context context
 				) throws IOException, InterruptedException {
 			int N_sum = 0;
@@ -55,51 +56,38 @@ public class PageRank {
 	}
 
 
-	public static class PageRankMapper 
+	public static class PageRankMapper
 	extends Mapper<Object,// input key
 	Text,  // input value
 	Text,  // output key
-	Text> //output value  
-	{ 
-		public void map(Object key, Text value, Context context) 
+	Text> //output value
+	{
+		@Override
+		public void map(Object key, Text value, Context context)
 				throws IOException, InterruptedException {
 			Configuration conf = context.getConfiguration();
 			String N = conf.get("N");    //passing parameters to Map
-			int count = Integer.parseInt(conf.get("Iteration"));
-			if (count >= 2) {
+			if (iterCount >= 0) {
 				String[] parts = value.toString().split(" ", 3);
-				// another solution:
-				// Text neighbour = null;
-				// if (parts.length > 2) {
-				//	neighbour.set(parts[2]);
-				//	String[] p_neighbour = parts[2].split(" ");
-				//	context.write(new Text(parts[0]), new Text(parts[2]));	
-				//	for (int p = 0; p < p_neighbour.length; p++){
-				//		context.write(new Text(p_neighbour[p]), new Text(String.valueOf(Double.parseDouble(parts[1])/p_neighbour.length)));
-				//	}	
-				//}
-				//context.write(new Text(parts[0]), new Text("#" + parts[1]));
-				//context.write(new Text(parts[0]), neighbour);	
 				if (parts.length == 2){
-					context.write(new Text(parts[0]), null);
+					context.write(new Text(parts[0]), new Text("$"));
 				} else if (parts.length == 3){
 					String[] p_neighbour = parts[2].split(" ");
 					//maybe you can use another mark to distinguish between PR and neighbour list, start with $start with $
-					context.write(new Text(parts[0]), new Text("$" + parts[2]));	
+					context.write(new Text(parts[0]), new Text("$" + parts[2]));
 					for (int p = 0; p < p_neighbour.length; p++){
 						context.write(new Text(p_neighbour[p]), new Text(String.valueOf(Double.parseDouble(parts[1])/p_neighbour.length)));
-					}	
+					}
 				}
 				// old page rank, start with #
 				context.write(new Text(parts[0]), new Text("#" + parts[1]));
 			}else{
-				// names not conflict in if and else, so you can use parts again in else 
+				// names not conflict in if and else, so you can use parts again in else
 				String[] parts_1 = value.toString().split(" ", 2);
 				double PR_part = 1 / Double.parseDouble(N);
-				context.write(new Text(parts_1[0]), new Text(Double.toString(PR_part) + parts_1[1]));
+				String neighbour = parts_1.length < 2 ? "" : " " + parts_1[1];
+				context.write(new Text(parts_1[0]), new Text(Double.toString(PR_part) + neighbour));
 			}
-
-
 		}
 
 	}
@@ -109,18 +97,23 @@ public class PageRank {
 		CONVERGE_COUNTER, OUTPUT_COUNTER
 	}
 
-	public static class PageRankReducer 
+	public static class PageRankReducer
 	//output value of reducer should be Text because it contains PR(double) and neighbour list(String)
 	extends Reducer<Text,Text,Text,Text> {
 		//result is IntWritable but sum should be double
-		//int type will truncate the numbers after decimal point 
+		//int type will truncate the numbers after decimal point
 		//for example  int a= 0.8 :   .8 will be discarded, so a = 0
+		@Override
 		public void reduce(Text key, Iterable<Text> values, //type of output value of mapper is Text
 				Context context
 				) throws IOException, InterruptedException {
 
+			if (iterCount < 0) {
+				context.write(key, values.iterator().next());
+				return;
+			}
 			Configuration conf = context.getConfiguration();
-			String N = conf.get("N_sum");//passing parameters to Reduce
+			String N = conf.get("N");//passing parameters to Reduce
 			// For a node A, There are 3 kinds of value pass to the reducer
 			// 1. Neighbour list of A(only one: either (A, null) or (A, the list)) starts with $
 			// 2. Old PageRank of A(only one), starts with #
@@ -130,23 +123,24 @@ public class PageRank {
 			String neighbourList = "";
 
 			for (Text val : values) {
+				if (val == null) continue;
 				String vs = val.toString();
 				if (vs.startsWith("#")) {
-					// sub-string start from 2nd character, namely remove # 
+					// sub-string start from 2nd character, namely remove #
 					oldPageRank = Double.parseDouble(vs.substring(1));
 				} else if (vs.startsWith("$")) { // this is the neighbour list
-					neighbourList = vs.substring(1); // discard $
+					neighbourList = vs.length() > 1 ? " " + vs.substring(1) : ""; // discard $
 				} else { // this is page rank from other nodes, add them up
-					sum += Double.parseDouble(vs);	
+					sum += Double.parseDouble(vs);
 				}
 
 			}
 			// convert string to double : Double.parseDouble(value)
 			double P_sum = (1-0.85)/Double.parseDouble(N) + 0.85 * sum;
 			//add space between pagerank and neighbour, otherwise you can't split them next round
-			context.write(key, new Text(Double.toString(P_sum) + " " + neighbourList));
+			context.write(key, new Text(Double.toString(P_sum) + neighbourList));
 
-			if (Math.abs(oldPageRank - P_sum) < 1e-8){ // ^ in java doesn't means exponent, 
+			if (Math.abs(oldPageRank - P_sum) < 1e-8){ // ^ in java doesn't means exponent,
 				context.getCounter(Counters.CONVERGE_COUNTER).increment(1); //access the counter and increment it when we change to dataset
 			}
 
@@ -155,13 +149,14 @@ public class PageRank {
 
 	}
 
-	public static class SortMapper 
-	extends Mapper<Text,// input key
+	public static class SortMapper
+	extends Mapper<Object,// input key
 	Text,  // input value
 	DoubleWritable,  // output key
-	Text> //output value  
-	{ 
-		public void map(Object key, Text value, Context context) 
+	Text> //output value
+	{
+		@Override
+		public void map(Object key, Text value, Context context)
 				throws IOException, InterruptedException {
 			String[] part = value.toString().split(" ", 2);
 			String[] value_part = part[1].split(" ");
@@ -172,7 +167,7 @@ public class PageRank {
 
 	public static class DescendingKeyComparator extends WritableComparator{
 		protected DescendingKeyComparator(){
-			super(Text.class, true);
+			super(DoubleWritable.class, true);
 		}
 
 		@SuppressWarnings("rawtypes")
@@ -184,9 +179,10 @@ public class PageRank {
 		}
 	}
 
-	public static class SortReducer 
+	public static class SortReducer
 	extends Reducer<DoubleWritable,Text,Text,DoubleWritable> {
 
+		@Override
 		public void reduce(DoubleWritable key, Iterable<Text> values, //type of output value of mapper is Text
 				Context context
 				) throws IOException, InterruptedException {
@@ -204,19 +200,17 @@ public class PageRank {
 						count++;
 					}
 				}
-				context.getCounter(Counters.CONVERGE_COUNTER).increment(count);
+				context.getCounter(Counters.OUTPUT_COUNTER).increment(count);
 			}
 		}
-
-
 
 	}
 
 
 
-	//	
+	//
 	public static void CalNDriver(String input, String output) throws IOException, ClassNotFoundException, InterruptedException {
-		Configuration conf = new Configuration(); 
+		Configuration conf = new Configuration();
 		Job job = new Job(conf);
 		job.setJarByClass(PageRank.class);
 		job.setMapperClass(CalNMapper.class);
@@ -225,13 +219,13 @@ public class PageRank {
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(input));
 		FileOutputFormat.setOutputPath(job, new Path(output));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		job.waitForCompletion(true);
 	}
 
 	public static boolean PageRankDriver(String input, String output) throws IOException, ClassNotFoundException, InterruptedException {
-		Configuration conf = new Configuration(); 
+		Configuration conf = new Configuration();
 		conf.set("N", N_sum); //conf.set: write into configuration file
-		conf.set("Iteration", Integer.toString(count));
+		conf.set("mapreduce.output.textoutputformat.separator", " ");
 		Job job = new Job(conf);
 		job.setJarByClass(PageRank.class);
 		job.setMapperClass(PageRankMapper.class);
@@ -241,11 +235,12 @@ public class PageRank {
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path(input));
 		FileOutputFormat.setOutputPath(job, new Path(output));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		job.waitForCompletion(true);
 
 		// convergence
-		long count = job.getCounters().findCounter(Counters.CONVERGE_COUNTER).getValue();
-		if (count < Long.parseLong(N_sum)) {
+		long convergeCount = job.getCounters().findCounter(Counters.CONVERGE_COUNTER).getValue();
+		if (convergeCount != oldConvergeCount) {
+			oldConvergeCount = convergeCount;
 			return true;
 		} else {
 			return false;
@@ -265,12 +260,12 @@ public class PageRank {
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path(input));
 		FileOutputFormat.setOutputPath(job, new Path(output));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		job.waitForCompletion(true);
 	}
 
 
 	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration(); 
+		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 		if (otherArgs.length != 2) {
 			System.err.println("Usage: pagerank <in> <out>");
@@ -291,17 +286,15 @@ public class PageRank {
 		BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(new Path(Nresult))));
 		N_sum = br.readLine(); // read a file from HDFS in Hadoop
 		PageRank.PageRankDriver(otherArgs[0], tmpDirName + "iter0");
+		iterCount++;
 		boolean iterate = true;
 		while (iterate) {
-			iterate = PageRank.PageRankDriver(tmpDirName + "iter" + count, tmpDirName + "iter" + (count+1));
-			count++;
+			iterate = PageRank.PageRankDriver(tmpDirName + "iter" + iterCount, tmpDirName + "iter" + (iterCount + 1));
+			iterCount++;
 		}
 		String sortOutput = tmpDirName + "sorttmp";
 		String sortResult = resultDirName + "pagerank.out";
-		PageRank.SortDriver(tmpDirName + "iter" + count, sortOutput);
+		PageRank.SortDriver(tmpDirName + "iter" + iterCount, sortOutput);
 		FileUtil.copyMerge(fs, new Path(sortOutput), fs, new Path(sortResult), false, conf, "");
 	}
 }
-
-
-
